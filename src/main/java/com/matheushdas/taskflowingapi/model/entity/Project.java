6cb70690fe615efd00b880ef01a31fbdd46dc46d
matheus-hdas <matheussantos.betim@gmail.com>
2024-12-20 19:13:34 -0300

    feat: add `Project` entity class

---
+++ src/main/java/com/matheushdas/taskflowingapi/model/entity/Project.java
@@ -0,0 +1,38 @@
+package com.matheushdas.taskflowingapi.model.entity;
+
+import com.matheushdas.taskflowingapi.model.utility.Status;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_project")
+@Data
+@NoArgsConstructor
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String description;
+    private String status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Project(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.status = Status.OPEN.getValue();
+    }
+}
